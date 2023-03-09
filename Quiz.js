17e8941feb030d4fea066c0110cd5441e5d66ed1
Tobias_sng <tobias.schilling@boehringer-ingelheim.com>
2023-03-09 14:08:29 +0100

    text schiebt sich nicht mehr unter footer

--- Quiz.js
+++ Quiz.js
@@ -1,7 +1,9 @@
-
+//Suchen der id="change-color-button"
 const changeColorButton = document.querySelector('#change-color-button');
+//Eveent hinzufügen(die Funktion wird erst ausgeführt, wenn der button geklickt wird)
 changeColorButton.addEventListener('click', function() {
   const islandCheckbox = document.querySelector('#island-checkbox');
+  //mit der if-Funktion festlegen, wann eine checkbox beim submitten grün wird und wann sie rot wird
   if (islandCheckbox.checked) {
     islandCheckbox.nextElementSibling.style.backgroundColor = 'red';
   } else {
@@ -40,5 +42,23 @@ changeColorButton.addEventListener('click', function() {
     gespaltenesHolz.nextElementSibling.style.backgroundColor = 'green';
   } else {
     gespaltenesHolz.nextElementSibling.style.backgroundColor = '';
+  }
+  const Datum1 = document.querySelector('#1492');
+  if (Datum1.checked) {
+    Datum1.nextElementSibling.style.backgroundColor = 'red';
+  } else {
+    Datum1.nextElementSibling.style.backgroundColor = '';
+  }
+  const Datum2 = document.querySelector('#1967');
+  if (Datum2.checked) {
+    Datum2.nextElementSibling.style.backgroundColor = 'red';
+  } else {
+    Datum2.nextElementSibling.style.backgroundColor = '';
+  }
+  const Datum3 = document.querySelector('#1850');
+  if (Datum3.checked) {
+    Datum3.nextElementSibling.style.backgroundColor = 'green';
+  } else {
+    Datum3.nextElementSibling.style.backgroundColor = '';
   }
 });
