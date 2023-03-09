646eb324b61efceda86f82da7af155ca7604ea6f
Tobias_sng <tobias.schilling@boehringer-ingelheim.com>
2023-03-09 09:20:56 +0100

    quiz

--- Quiz.js
+++ Quiz.js
@@ -1,26 +1,44 @@
-function Ergebnis(){
-    const ski = document.querySelector('input[name="Ski"]').value;
-    const norway = document.querySelector('input[name="Norwegen"]').checked;
-    const Schweden = document.querySelector('input[name="Schweden"]').checked;
-    const Island = document.querySelector('input[name="Island"]').checked;
-    const scheit = document.querySelector('input[name="Scheit"]').checked;
-    const holzbrett = document.querySelector('input[name="Holzbrett"]').checked;
-    const gespalten = document.querySelector('input[name="gespaltenes Holz"]').checked;
-    var norwegen = document.getElementById("norwegen");
-    var Scheit = document.getElementById("Scheit");
-    var Gespalten = document.getElementById("gespalten");
 
+const changeColorButton = document.querySelector('#change-color-button');
+changeColorButton.addEventListener('click', function() {
+  const islandCheckbox = document.querySelector('#island-checkbox');
+  if (islandCheckbox.checked) {
+    islandCheckbox.nextElementSibling.style.backgroundColor = 'red';
+  } else {
+    islandCheckbox.nextElementSibling.style.backgroundColor = '';
+  }
 
-    if((norway && Schweden && Island) ||(norway && Island && !Schweden)||(Island && Schweden && !Norwegen)||(norway && Schweden && !Island)){
-        alert("sie dürfen nur ein Kästchen ausfüllen")
-    };
-    if(norway && !Schweden && !Island){
-        norwegen.classList.add("right");
-    };
-    if(scheit && !holzbrett && gespalten){
-        Scheit.classList.add("right");
-        Gespalten.classList.add("right");
-    };
-  
+  const norwegenCheckbox = document.querySelector('#norwegen-checkbox');
+  if (norwegenCheckbox.checked) {
+    norwegenCheckbox.nextElementSibling.style.backgroundColor = 'green';
+  } else {
+    norwegenCheckbox.nextElementSibling.style.backgroundColor = '';
+  }
 
-};
+  const schwedenCheckbox = document.querySelector('#schweden-checkbox');
+  if (schwedenCheckbox.checked) {
+    schwedenCheckbox.nextElementSibling.style.backgroundColor = 'red';
+  } else {
+    schwedenCheckbox.nextElementSibling.style.backgroundColor = '';
+  }
+  const scheit =document.querySelector('#scheit')
+  if (scheit.checked) {
+    scheit.nextElementSibling.style.backgroundColor = 'green';
+  } else {
+    scheit.nextElementSibling.style.backgroundColor = '';
+  }
+
+  const holzbrett = document.querySelector('#holzbrett');
+  if (holzbrett.checked) {
+    holzbrett.nextElementSibling.style.backgroundColor = 'red';
+  } else {
+    holzbrett.nextElementSibling.style.backgroundColor = '';
+  }
+
+  const gespaltenesHolz = document.querySelector('#gespalten');
+  if (gespalten.checked) {
+    gespaltenesHolz.nextElementSibling.style.backgroundColor = 'green';
+  } else {
+    gespaltenesHolz.nextElementSibling.style.backgroundColor = '';
+  }
+});
