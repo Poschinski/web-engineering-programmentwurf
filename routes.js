9576f55cc1a7dd3432d81d391c3d67e64087d417
Tobias_sng <tobias.schilling@boehringer-ingelheim.com>
2023-03-14 11:33:39 +0100

    routes kommentiert

--- routes.js
+++ routes.js
@@ -4,29 +4,29 @@ const router = express.Router();
 const { getUsers, writeUsers, getArticles, writeArticles } = require('./data');
 
 let users = getUsers();
-
+// http://localhost:3000/Artikel-Bodybuilding
 router.get('/bodybuilding/:name(entspannung|grundlagen|protein)', (req, res) => {
     let name = req.params.name
     let comments = getComments(name)
     res.render("pages/bodybuilding/" + name, { comments: comments });
 })
-
+// http://localhost:3000/Artikel-Klettern
 router.get('/klettern/:name(einfuehrung|klettergebiete|varianten)', (req, res) => {
     let name = req.params.name
     let comments = getComments(name)
     res.render("pages/klettern/" + name, { comments: comments });
 })
-
+// http://localhost:3000/Artikel-Skisport
 router.get('/skifahren/:name(history|olympia|worldcup)', (req, res) => {
     let name = req.params.name
     let comments = getComments(name)
     res.render("pages/skifahren/" + name, { comments: comments });
 })
-
+// http://localhost:3000/registrierungsseite
 router.get("/registration", (req, res) => {
     res.render("pages/registration");
   });
-  
+// http://localhost:3000/loginseite
 router.get("/login", (req, res) => {
     res.render("pages/login");
 });
@@ -36,17 +36,19 @@ router.get('/klettern', (req, res) => {
     res.render("pages/climbing-overview");
 })
 
-// http://localhost:3000/
+// http://localhost:3000/startseite
 router.get('/', (req, res) => {
     res.render("pages/startseite");
 })
+// http://localhost:3000/quiz
 router.get('/quiz', (req, res) => {
     res.render("pages/quiz");
 })
+// http:localhost:3000/skifahren
 router.get('/skifahren', (req, res) => {
     res.render("pages/ski-overview");
 })
-
+// http:localhost:3000/bodybuilding
 router.get('/bodybuilding/', (req, res) => {
     res.render("pages/bodybuilding-overview");
 })
